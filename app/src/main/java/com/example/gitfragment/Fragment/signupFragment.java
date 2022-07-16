238757992b031f9e6e781107a3817f2253474334
azmoh09 <93793786+azmoh09@users.noreply.github.com>
2022-07-16 22:57:41 +0300

    feat: add signup fragment

---
+++ app/src/main/java/com/example/gitfragment/Fragment/signupFragment.java
@@ -0,0 +1,48 @@
+package com.example.gitfragment.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gitfragment.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link signupFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class signupFragment extends Fragment {
+
+
+
+    public signupFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+
+     * @return A new instance of fragment signupFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_signup, container, false);
+    }
+}
